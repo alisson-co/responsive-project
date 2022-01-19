aae374b561977cb99af1bb30c8ee560c305ee7e4
alisson.co <alisson_oliveira_11@hotmail.com>
2022-01-19 19:51:40 -0300

    update JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar-menu');
+const navLogo = document.querySelector('#navbar-logo')
 
 /* display mobile menu */
 const mobileMenu = () => {
@@ -9,35 +10,35 @@ const mobileMenu = () => {
 
 menu.addEventListener('click', mobileMenu);
 
-const highlightMenu = () =>{
-  const elem = document.querySelector('.highlight')
-  const homeMenu = document.querySelector('#home-page')
-  const aboutMenu = documento.querySelector('#about-page')
-  const serviceMenu = document.querySelector('#services-page')
-  let scrollPos = window.scrollPos
+const highlightMenu = () => {
+  const elem = document.querySelector('.highlight');
+  const homeMenu = document.querySelector('#home-page');
+  const aboutMenu = document.querySelector('#about-page');
+  const servicesMenu = document.querySelector('#services-page');
+  let scrollPos = window.scrollY;
 
-  if(window.innerWidht > 960 && scrollPos <600){
-    homeMenu.classList.add('highlight')
-    aboutMenu.classList.remove('highlight')
-    return
+  if (window.innerWidth > 960 && scrollPos < 600) {
+    homeMenu.classList.add('highlight');
+    aboutMenu.classList.remove('highlight');
+    return;
   } else if(window.innerWidth > 960 && scrollPos <1400){
-    aboutMenu.classList.add('highlight')
-    homeMenu.classList.remove('highlight')
-    serviceMenu.classList.remove('highligh')
-    return
+    aboutMenu.classList.add('highlight');
+    homeMenu.classList.remove('highlight');
+    servicesMenu.classList.remove('highlight');
+    return;
   } else if(window.innerWidth > 960 && scrollPos <2345){
-    serviceMenu.classList.add('highlight')
-    aboutMenu.classList.remove('highlight')
-    return
+    servicesMenu.classList.add('highlight');
+    aboutMenu.classList.remove('highlight');
+    return;
   }
 
-  if((elem && window.innerWidth < 960 && scrollPos) || elem){
-    elem.classList.remove('highlight')
+  if ((elem && window.innerWIdth < 960 && scrollPos < 600) || elem) {
+    elem.classList.remove('highlight');
   }
 }
 
-window.addEventListener('scroll', highlightMenu)
-window.addEventListener('click', highlightMenu)
+window.addEventListener('scroll', highlightMenu);
+window.addEventListener('click', highlightMenu);
 
 const hideMobileMenu = () => {
   const menuBars = document.querySelector('is-active')
